2e0cb9ac1b13417c90d0156babe0ac13822977d9
Ali Babaee <alisource79@gmail.com>
2025-01-24 01:02:52 +0330

    App repo initialized.

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const showDialog = ref(false);
+</script>
+
+<template>
+  <div class="container">
+    Hello
+  </div>
+</template>
+
+<style lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
